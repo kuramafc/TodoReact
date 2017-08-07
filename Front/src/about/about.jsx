8faa06dac0af35e7626790202e7cbace60075b08
bruno.giammella@sciensa.com <bruno.giammella@sciensa.com>
2017-08-07 13:36:56 -0300

    React without Redux

---
+++ Front/src/about/about.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+         <PageHeader name='Sobre' small='Nós'></PageHeader>
+         <h2>xablau</h2>
+         <p>Reina</p>
+         <h2>xablau</h2>
+         <p>Reina</p>
+         <h2>xablau</h2>
+         <p>Reina</p>
+         <h2>xablau</h2>
+         <p>Reina</p>
+    </div>
+)
